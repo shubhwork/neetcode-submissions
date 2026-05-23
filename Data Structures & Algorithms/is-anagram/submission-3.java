87f4f4a6e9bd0bd1ba9bf18102102ab93618de20
shubhwork <85920162+shubhwork@users.noreply.github.com>
2026-05-23 11:57:01 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int [] arr=new int[26];
+        for(char c:s.toCharArray()){
+            arr[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            arr[c-'a']--;
+        }
+        for(int i:arr){
+            if(i!=0)return false;
+        }
+        return true;
+    }
+}
