1867168f605463379255680739c89b344ec913aa
shubhwork <85920162+shubhwork@users.noreply.github.com>
2026-06-17 11:01:12 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+      Map<String,List<String>> map=new HashMap<>();
+      for(String s:strs){
+        char ch[]=s.toCharArray();
+        Arrays.sort(ch);
+        String str=new String(ch);
+        if(!map.containsKey(str)){
+            map.put(str,new ArrayList<>());
+        }
+        map.get(str).add(s);
+      }
+      return new ArrayList<>(map.values());
+
+    }
+}
