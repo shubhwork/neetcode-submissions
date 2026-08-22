d04d4994dd451fef7323f17591070bf89f15f6f9
shubhwork <85920162+shubhwork@users.noreply.github.com>
2026-08-22 19:55:13 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        Map<Character,Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)-1);
+        }
+        for(char c:s.toCharArray()){
+            if(map.get(c)!=0)return false;
+        }
+        return true;
+    }
+}
